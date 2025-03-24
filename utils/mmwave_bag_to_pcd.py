6e8256bf13c45751a979d715daf77f9b592abcb8
solidchrysalis <musicemail7171@gmail.com>
2025-03-23 23:23:00 -0400

    Finalized pcd saver

--- utils/mmwave_bag_to_pcd.py
+++ utils/mmwave_bag_to_pcd.py
@@ -1,67 +1,41 @@
+import open3d 
 import pypcd4
 import numpy as np
-import bagpy
+import bagreader as bag
+import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2, PointField
 
 def pointcloud_bag(filename):
     tstart = None
     tend = None
-    type_to_look ="laser/scan"
-    b = bagreader(filename)
-    print(b)
-    """
+    type_to_look ="sensor_msgs/PointCloud2"
+    b = bag.bagreader(filename)
+    print(b.topic_table)
     table_rows = b.topic_table[b.topic_table['Types']==type_to_look]
     topics_to_read = table_rows['Topics'].values
     message_counts = table_rows['Message Count'].values
-    print(message_counts)
+    #print(topics_to_read)
     
-    column_names = [
-                            "headser.seq", 
-                            "header.frame_id", 
-                            "height"
-                            "width"
-                            "fields.name"
-                            "fields.offset"
-                            "fields.datatype"
-                            "fields.count"
-                            "is_bigendian"
-                            "point_step"
-                            "row_step"
-                            "data"
-                            "is_dense"
-                            ]
-
-    array = []
+    point_cloud_data = []
     for i in range(len(table_rows)):
-        for topic, msg, t in self.reader.read_messages(topics=topics_to_read[i], start_time=tstart, end_time=tend):
-            new_row = [t.secs + t.nsecs*1e-9, 
-                                    msg.header.seq, 
-                                    msg.header.frame_id,
-                                    msg.child_frame_id,
-                                    msg.pose.pose.position.x,
-                                    msg.pose.pose.position.y,
-                                    msg.pose.pose.position.z,
-                                    msg.pose.pose.orientation.x,
-                                    msg.pose.pose.orientation.y,
-                                    msg.pose.pose.orientation.z,
-                                    msg.pose.pose.orientation.w,
-                                    msg.twist.twist.linear.x,
-                                    msg.twist.twist.linear.y,
-                                    msg.twist.twist.linear.z]
+        for topic, msg, t in b.reader.read_messages(topics="/rio/combined_radar_scan_2", start_time=tstart, end_time=tend):
+            points = list(pc2.read_points(msg, field_names=('x', 'y', 'z'), skip_nans=True))
+            point_cloud_data.extend(points)
+    # print(point_cloud_data)
+    # Convert list to a NumPy array
+    cloud_array = np.array(point_cloud_data, dtype=np.float32)
 
-    array.append(file_to_write)
-    return csvlist
-    """
+    # Convert to Open3D format
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(point_cloud_data)
+
+    # Save as a .pcd file
+    o3d.io.write_point_cloud("output.pcd", pcd)
+    print("Saved point cloud to output.pcd")     
 
 def main():
-    pointcloud_bag("video_log_2025-03-05-18-38-28.bag")
+    pointcloud_bag("../test/rio_log_2025-03-05-18-38-28.bag")
 
 if __name__ == "__main__":
     main()
 
-
-
-
-
-if __name__ == "__main__": 
-	main()
